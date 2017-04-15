f083a57b540f3767c437dfc63ca68bfc053e8605
Levi <levi@levibrooke.com>
2017-04-14 21:08:23 -1000

    Work on exercises

--- exercises.js
+++ exercises.js
@@ -1,46 +1,75 @@
 /*
 Objects in Javascript can be compared to objects in real life. Objects has a collection of properties associated with it (known as key-value pairs) and are basically the same as ordinary variables.
-*/ 
+*/
 
 
 /*
-A literal object. 
+A literal object.
    Declare a variable named `dog` and create the following properties (key-value pairs) to the object:
-   
+
     - add a key named `name` and set it to a string value of your choice
     - add a key named `age` and set it to a number value of your choice
     - add a key named `vegeterian` and set it to a boolean value of your choice
     - add a key named `color` and set it to an array with two string elements in it
-    - add a key named `speak` which will be a method that return a string "woof!"; 
-    
- Console.log the following phrase by accessing the appropriate object properties:
- 
- "My dog `name` is `age` year old likes to `speak`."
- */ 
+    - add a key named `speak` which will be a method that return a string "woof!";
 
+ Console.log the following phrase by accessing the appropriate object properties:
+
+ "My dog `name` is `age` year old likes to `speak`."
+ */
+
+var dog = {
+  name: "Brinkley",
+  age: 15,
+  vegetarian: false,
+  color: ["brown", "black"],
+  speak: function(){
+    return "woof!";
+  }
+};
+
+console.log("My dog " + dog.name + " is " + dog.age + " years old likes to " + dog.speak());
 
 /*
 An empty object
    Declare a variable named `kicks` and assign it to an EMPTY object.
    Next you will add the following properties to this object. Each property declaration expression will be on its own line and
-   ending with semicolons (`;`). 
+   ending with semicolons (`;`).
             - add a property named `brand` and set it to your favorite sneaker brand
             - add a property named `color` and set it the value "red"
             - add a property named `size` and set it to your shoe size
             - add a property named `buy` and set it to a method that will return a string "Hell Yeah!"
-            
+
   Console.log the object.
 */
 
+var kicks = {};
 
-/* 
+kicks.brand = "Puma";
+kicks.color = "red";
+kicks.size = 10;
+kicks.buy = function(){
+  return "Hell Yeah!";
+};
+
+console.log(kicks);
+console.log(kicks.buy());
+
+/*
    Declare a variable named `plainBox` and set its value to be an EMPTY object.
-   Next, you will add three properties to this object. 
+   Next, you will add three properties to this object.
             - add a property named `color` and set it the name of any color.
             - add a property named `size` and set it to a number value between `0` and `20`.
             - add a property named `contents` and set it to be an empty array.
 */
 
+var plainBox = {};
+
+plainBox.color = "green";
+plainBox.size = 13;
+plainBox.contents = [];
+
+console.log(plainBox);
 
 /*
    Declare a variable named `stockCar` and create the following properties (key-value pairs) to the object:
@@ -51,22 +80,47 @@ An empty object
             - add a key named `passengers` and set it to be an empty array.
 */
 
+var stockCar = {
+  model: "Ford",
+  year: 2005,
+  automaticTransmission: true,
+  driver: null,
+  passengers: [],
+};
+
+console.log(stockCar);
 
 /*
    Add new property inside a function
    Declare a variable named `plainPerson`. Set its value to be an EMPTY object with no properties.
 
-   Next, declare a function named `buildPerson` and then define three parameters. The first parameter will be named `person` 
-   and the second will be `nameString` and the third will be `age`. Within this function you will modify the `person` object 
+   Next, declare a function named `buildPerson` and then define three parameters. The first parameter will be named `person`
+   and the second will be `nameString` and the third will be `age`. Within this function you will modify the `person` object
    by:
             - adding a property called `name` and set its value to be the second parameter.
             - adding a property called `age` and set its value to be the third parameter.
 
    The function will return the first parameter.
-   Finally, invoke your new function while passing in the `plainPerson` object and additional agrument values, then store 
-   the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just 
+   Finally, invoke your new function while passing in the `plainPerson` object and additional agrument values, then store
+   the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just
    the value at `name`, and just the value at `age`.
  */
+
+var plainPerson = {};
+
+function buildPerson(person, nameString, age){
+  person.name = nameString;
+  person.age = age;
+  return person;
+}
+
+buildPerson(plainPerson, "Levi Porter", 21);
+console.log(plainPerson);
+
+var completePerson = buildPerson(plainPerson, "Levi Porter", 21);
+console.log(completePerson);
+console.log(completePerson.name);
+console.log(completePerson.age);
 
 
 /*
@@ -74,7 +128,8 @@ An empty object
    Declare a new variable named `arrayOfObjects` and set it to be [this array of objects
    (https://gist.github.com/sgnl/958adf99007329d2e4ff).
 
-   Declare a new function named `printProcessedOrders`and set one parameter called `orders`. Within this function you 
+
+   Declare a new function named `printProcessedOrders`and set one parameter called `orders`. Within this function you
    will return a string that prints the order details to the page in the following format.
 
         Example output:
@@ -94,24 +149,105 @@ An empty object
             ...
  */
 
+ var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
+
+// console.log(arrayOfObjects);
+
+function printProcessedOrders(orders){
+  // console.log(arrayOfObjects);
+  for (var i = 0; i < orders.length; i++) {
+    // console.log(orders[i]);
+    console.log("====");
+    console.log("id: ", orders[i].id);
+    console.log("purchase date: ", orders[i].date);
+    console.log("purchase total: ", orders[i].total);
+  }
+}
+
+printProcessedOrders(arrayOfObjects);
+
 
 /*
    Addition with an object
-   Declare a new variable named sumObj and set it to be a new object with the properties `a`, `b`, and `result`. 
+   Declare a new variable named sumObj and set it to be a new object with the properties `a`, `b`, and `result`.
    Properties 'a' and 'b' will be set to a number and result will be 'undefined.'
 
-   Declare a new function named objectAddition. Declare the first parameter, which will be an object. Within this 
-   function, you will access the values stored `a` and `b` of the object being passed in. Add the these values and store 
+   Declare a new function named objectAddition. Declare the first parameter, which will be an object. Within this
+   function, you will access the values stored `a` and `b` of the object being passed in. Add the these values and store
    the sum to this object's `result` property. This function will return the object.
 
-        Invoke your function and pass in your object, store the result to a variable named sumObjResult and use `console.log` 
+        Invoke your function and pass in your object, store the result to a variable named sumObjResult and use `console.log`
         to inspect your results.
 */
 
+var sumObj = {
+  a: 10,
+  b: 5,
+  result: undefined,
+};
+
+function objectAddition(object){
+  sumObj.result = sumObj.a + sumObj.b;
+}
+
+objectAddition(sumObj);
+var sumObjResult = sumObj.result;
+console.log(sumObjResult);
 
 /*
    Print sum function and add as new key-value
-   Declare a new function named printObj and a single parameter which will be the object from the challenge just above. 
+   Declare a new function named printObj and a single parameter which will be the object from the challenge just above.
    Within this function you are to print to the screen a message of the operation performed. For Example:
 
         // if this object was passed into your function:
@@ -119,48 +255,97 @@ An empty object
         // it should print a message saying
         '10 + 67 = 77'
 
-        Before returning this object, add a new property to it named `output` and set it to be the message that was printed 
+        Before returning this object, add a new property to it named `output` and set it to be the message that was printed
         out.
 
-        Invoke this function and pass in your object. Further test by changing the values of the object being passed in or 
+        Invoke this function and pass in your object. Further test by changing the values of the object being passed in or
         **create more** objects and invoke your function multiple times.
  */
+
+ function printObj(obj){
+   obj.result = obj.a + obj.b;
+   obj.a + "+" + obj.b + "=" + obj.result;
+ }
+
+ console.log();
 
 
 /*
    Putting stuff in `plainBox`
-        Declare a function named putInPlainBox and a single parameter which will be an object. Within this function, write a 
-        FOR loop that adds **10** random number values to the array referenced at the `contents` property of the object being 
+        Declare a function named putInPlainBox and a single parameter which will be an object. Within this function, write a
+        FOR loop that adds **10** random number values to the array referenced at the `contents` property of the object being
         passed in. This function will return the object.
 
-        Invoke your function and pass in your object (which should be `plainBox`), store the result to a variable named 
+        Invoke your function and pass in your object (which should be `plainBox`), store the result to a variable named
         plainBoxResult and use `console.log` to inspect your results.
  */
 
+ // var plainBox = {};
+ //
+ // plainBox.color = "green";
+ // plainBox.size = 13;
+ // plainBox.contents = [];
+
+function putInPlainBox(obj){
+  for (var i = 0; i < 11; i++) {
+    obj.contents.push(Math.floor(Math.random()* 10 + 1));
+  }
+  return obj;
+}
+
+var plainBoxResult = putInPlainBox(plainBox);
+console.log(plainBoxResult);
 
 /*
    Detecting transmission
-        Declare a function named detectingTransmission and a single parameter which will be an object. Within this function 
+        Declare a function named detectingTransmission and a single parameter which will be an object. Within this function
         you will check to see if the car has an automatic or manual transmission and print the results on screen.
 
-        If `automaticTransmission` is true then print a message saying so. Also, provide an appropriate message for when the 
+        If `automaticTransmission` is true then print a message saying so. Also, provide an appropriate message for when the
         its false.
 
         Invoke your function and pass in your stockCar object, store the result to a variable named isAutomaticTransmission and use `console.log` to inspect your results.
  */
 
+ // var stockCar = {
+ //   model: "Ford",
+ //   year: 2005,
+ //   automaticTransmission: true,
+ //   driver: null,
+ //   passengers: [],
+ // };
+
+function detectingTransmission(obj){
+  if (obj.automaticTransmission === true) {
+    return "This car has an automatic transmission";
+  }
+  else {
+    return "This car has a stick shift";
+  }
+}
+
+var isAutomaticTransmission = detectingTransmission(stockCar);
+console.log(isAutomaticTransmission);
 
 /*
    Who's driving this thing?!
         As you may have noticed that the `stockCar` doesn't have a driver!
 
-        Declare a function named addDriver with two parameters. The first parameter will be an object with represents a 
-        **car**, the other will be a **person**. Within this function, set the `driver` value of the **stockCar** to the 
+        Declare a function named addDriver with two parameters. The first parameter will be an object with represents a
+        **car**, the other will be a **person**. Within this function, set the `driver` value of the **stockCar** to the
         second parameter being passed into your function.
 
-        Invoke your function and pass in your objects, store the result to a variable named stockCarWithDriver, and inspect 
+        Invoke your function and pass in your objects, store the result to a variable named stockCarWithDriver, and inspect
         your results. Consider using `plainPerson` as your driver.
  */
+
+function addDriver(obj, person){
+  obj.driver = person;
+  return obj;
+}
+
+var stockCarWithDriver = addDriver(stockCar, "Jeff Gordon");
+console.log(stockCarWithDriver);
 
 
 /*
